37b447c4d5c52608e82ccf96e0c5ae0fc7de95b1
angela <angelahuima@gmail.com>
2025-04-29 15:23:20 -0500

    desafio 2

---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+export const Navbar= () =>{
+    return (
+        <div className="container">
+            <nav className="nav">
+                <div className="nav__brand">
+                    <a className="nav__link" href="#">MiMarca</a>
+                </div>
+                <ul className="nav__list">
+                    <li>
+                        <a className="nav__link" href="#">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a className="nav__link" href="#">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a className="nav__link" href="#">Carrito</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+export default Navbar;
